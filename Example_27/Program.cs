daab38a1a90aa47a50189bd35f74b40a60139533
Svetlana Movchan <movchansvetlana@bk.ru>
2023-06-27 02:19:20 +0300

    Сумма всех чисел в цифре

---
+++ Example_27/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+void Print (string text){  
+   Console.Write(text); 
+}
+
+int GetIntNum(){
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int SumAllDigit(int number){
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+Print("Введите число => ");
+int number = GetIntNum();
+Print($"Сумма всех чисел в цифре {number} = {SumAllDigit(number)}");
